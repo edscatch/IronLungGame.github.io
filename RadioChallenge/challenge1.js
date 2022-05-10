704b5a1597c5a0c544f11677b4a580941d20bbc1
edscat <edscat101@gmail.com>
2022-05-10 22:45:33 +1000

    fucked with forms n shit yet again

--- RadioChallenge/challenge1.js
+++ RadioChallenge/challenge1.js
@@ -3,13 +3,15 @@ function getAnswer() {
     let link = "google.com/"
     if (x == 10) {
         document.getElementById("nextPage").href = link;
-        document.getElementById("nextPage").innerHTML = ("Next page");
+        document.getElementById("nextPage").innerHTML = ("Next page"); //FIXME change link to actual page
         document.getElementById("nextPage").className = ("nextPageLink");
+        document.getElementById("inputForm").hidden = true;
         return false;
-    } else if ((x > 0) || (x == "")){
-       document.getElementById("hiddentext").innerHTML = ("Please enter a number above 0")
-        return false;
+    //} else if (x > 0){
+       //document.getElementById("hiddentext").innerHTML = ("Please enter a number above 0");
+       //console.log("i hope this doesn't fucking trigger");
+       // return false;
     } else {
-        return true;
+            return true;
     }
 }
