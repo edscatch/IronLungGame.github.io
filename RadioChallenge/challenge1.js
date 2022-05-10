c6f7777352f7b2a6ea023617ec881d58a4373d2d
edscatch <s3944106@student.rmit.edu.au>
2022-05-11 00:31:47 +1000

    hiding text is harder then it should be + added files

--- RadioChallenge/challenge1.js
+++ RadioChallenge/challenge1.js
@@ -1,11 +1,15 @@
+document.getElementById("correctText").hidden = true; //FIXME figure out how to hide the text. or hardcode it.
+console.log("all hail sebastian")
 function getAnswer() {
     let x = document.forms["answerForm"]["userAnswer"].value;
-    let link = "google.com/"
+    let link = "/challenge2.html"
     if (x == 10) {
         document.getElementById("nextPage").href = link;
-        document.getElementById("nextPage").innerHTML = ("Next page"); //FIXME change link to actual page
+        document.getElementById("nextPage").innerHTML = ("Next page"); 
         document.getElementById("nextPage").className = ("nextPageLink");
         document.getElementById("inputForm").hidden = true;
+        document.getElementById("userAnswer").hidden = true;
+        document.getElementById("correctText").hidden = false;
         return false;
     //} else if (x > 0){
        //document.getElementById("hiddentext").innerHTML = ("Please enter a number above 0");
