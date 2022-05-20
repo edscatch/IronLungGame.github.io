6843eab63b353910ecfffb113674ef3abcf6d252
edscat <edscat101@gmail.com>
2022-05-20 12:37:23 +1000

    added folder for java challenges

---
+++ JavaChallenges/RadioChallenge2.java
@@ -0,0 +1,11 @@
+public class RadioChallenge2{
+    public static void main(String[] args) {
+        int[] intArray = new int[3];
+        intArray[0] = 23;
+        intArray[1] = 42;
+        intArray[3] = 12;
+        intArray[4] = 17;
+        System.out.println(intArray[4]);
+    }
+}
+// answer is error or the 3rd option.
